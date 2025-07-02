93c6e53a39a0370e4e670f8e040ef8507e1807c7
Ronshy <michael.evanuel.saptomo@gmail.com>
2025-07-02 12:02:33 +0700

    Add Supabase configuration file
    Create a new config.php file to define Supabase URL and API key for backend usage, ensuring sensitive keys are not exposed in the frontend.

---
+++ backend/config.php
@@ -0,0 +1,6 @@
+<?php
+// Ganti sesuai project Supabase kamu
+define('SUPABASE_URL', 'https://nmgwcavicouhekywqkpt.supabase.co');
+
+// HATI-HATI! Gunakan service_role key HANYA di backend, JANGAN di frontend
+define('SUPABASE_API_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tZ3djYXZpY291aGVreXdxa3B0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTM1NDYxNywiZXhwIjoyMDY2OTMwNjE3fQ.0000000000000000000000000000000000000000000000000000000000000000');
